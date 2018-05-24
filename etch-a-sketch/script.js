6d0ba23171034ce3c8f1aea7fa6524049ac7a466
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-24 11:47:08 -0400

    Basic etch-a-sketch working

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -3,17 +3,28 @@ container.classList.add('container');
 
 // Build grid
 let grid = [];
-for (let i = 1; i <= 4; i++) {
-	for (let j = 1; j <= 4; j++) {
-		grid[j] = document.createElement('div');
-		grid[j].setAttribute('id', 'square' + i + '-' + j);
-		grid[j].classList.add('square');
-		if (j == 1) {
-			grid[j].classList.add('row');
-		}
-		container.appendChild(grid[j]);
+for (let i = 0; i < 16; i++) {
+	grid[i] = document.createElement('div');
+	grid[i].setAttribute('id', 'square' + i);
+	grid[i].classList.add('square');
+
+	// Draw on hover
+	grid[i].addEventListener('mouseover', draw);
+
+	// Begin new row
+	if (i % 4 == 0) {
+		grid[i].classList.add('row');
 	}
+
+	container.appendChild(grid[i]);
 }
+
+console.log(grid);
 
 // Display grid
 document.body.appendChild(container);
+
+function draw() {
+	console.log(this);
+	this.classList.add('hovered');
+}

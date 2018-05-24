b5f42390167f3ff4190fff042a3155d15a82d4c9
Ricky McCallum <rickymccallum87@gmail.com>
2018-05-24 14:47:27 -0400

    Center grid, change colors

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,24 +1,26 @@
 let gridContainer = document.createElement('div');
+gridContainer.setAttribute('style', 'width: ' + (50 * 4 + 2 * 4) + 'px;');
+gridContainer.classList.add('mx-auto');
 
 // Build grid
 let grid = [];
 for (let i = 0; i < 16; i++) {
 	grid[i] = document.createElement('div');
-	grid[i].setAttribute('id', 'square' + i);
+	// grid[i].setAttribute('id', 'square' + i);
 	grid[i].classList.add('square');
+	grid[i].setAttribute('style', 'width: ' + 50 + 
+		'px; height: ' + 50 + 'px;');
 
 	// Draw on hover
 	grid[i].addEventListener('mouseover', draw);
 
 	// Begin new row
 	if (i % 4 == 0) {
-		grid[i].classList.add('row');
+		grid[i].classList.add('newRow');
 	}
 
 	gridContainer.appendChild(grid[i]);
 }
-
-console.log(grid);
 
 // Display grid
 sketchPad.appendChild(gridContainer);

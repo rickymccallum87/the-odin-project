65d696b231b360ecd4a5b10aa2bd58c7192f2df7
Ricky McCallum <rickymccallum87@gmail.com>
2018-07-16 12:41:24 -0400

    Attempt to add event listeners

--- calculator/script.js
+++ calculator/script.js
@@ -1,28 +1,48 @@
-display = document.getElementById('display');
-keys = document.getElementsByClassName('btn');
+let display = document.getElementById('display');
+let buttons = document.querySelectorAll('button');
 
-function input() {
-	display.textContent.concat(this.textContent);
+for (let i = 0; buttons.length < i; i++) {
+	buttons[i].addEventListener('click', console.log('clicked'));
 }
 
-function add(op1, op2) {
-	return op1 + op2;
+// Math operations
+let add = (op1, op2) => op1 + op2;
+let subtract = (op1, op2) => op1 - op2;
+let multiply = (op1, op2) => op1 * op2;
+let divide = (op1, op2) => op1 / op2;
+let operate = (opr, op1, op2) => opr(op1, op2);
+
+// React to click, depending which button
+function input(btn) {
+	switch (btn) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			display.textContent += btn;
+			break;
+		case 'clear':
+			display.textContent = '';
+			break;
+		case 'add':
+			break;
+		default:
+			alert('Error: Key not recognized.');
+	}
 }
 
-function subtract(op1, op2) {
-	return op1 - op2;
-}
+// let btnClr = document.getElementById('clear');
+// btnClr.onclick = function() {
+// 	display.textContent = '';
+// }
 
-function multiply(op1, op2) {
-	return op1 * op2;
-}
-
-function divide(op1, op2) {
-	return op1 / op2;
-}
-
-function operate(opr, op1, op2) {
-	return opr(op1, op2);
-}
-
-
+// let btn7 = document.getElementById('7');
+// btn7.onclick = function() {
+// 	display.textContent += '7';
+// }

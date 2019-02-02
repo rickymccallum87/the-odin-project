e9c8383016710956f5534539acb239a929bb25e8
Ricky McCallum <contact@rickymccallum.com>
2019-02-02 11:12:04 -0500

    Shift string characters

--- ruby-building-blocks/caesar_cipher.rb
+++ ruby-building-blocks/caesar_cipher.rb
@@ -1,6 +1,11 @@
 def caesar_cipher(text, shift)
-  ascii_codes = text.split('').map! { |char| char.ord }
-  puts ascii_codes.inspect
+
+  ascii_codes = text.split('').map do |char|
+    (char.ord + shift).chr
+  end
+  
+  puts ascii_codes.join
+
 end
 
 caesar_cipher("What a string!", 5)

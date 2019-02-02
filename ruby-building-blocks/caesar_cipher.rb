99fbf28ab74cc06590b3e3a4d9804443be64c4b5
Ricky McCallum <contact@rickymccallum.com>
2019-02-02 11:29:58 -0500

    Distinguish between capital and lowercase letters

--- ruby-building-blocks/caesar_cipher.rb
+++ ruby-building-blocks/caesar_cipher.rb
@@ -1,10 +1,23 @@
 def caesar_cipher(text, shift)
 
-  ascii_codes = text.split('').map do |char|
-    (char.ord + shift).chr
+  shifted_chars = text.split('').map do |char|
+    ascii = char.ord
+    shifted_ascii = ascii + shift
+
+    # A-Z = 65-90, a-z = 97-122
+    if ascii >= 65 and ascii <= 90 # upper case
+      print 'X'
+    elsif ascii >= 97 and ascii <= 122 # lower case
+      print 'x'
+    else # non-alphabet
+      print ' '
+    end
+
+    shifted_ascii.chr
   end
+  puts
   
-  puts ascii_codes.join
+  puts shifted_chars.join
 
 end
 

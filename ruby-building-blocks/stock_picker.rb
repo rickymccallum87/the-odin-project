87a869d1c06132570c0514005b7d43eb3eaf4c51
Ricky McCallum <contact@rickymccallum.com>
2019-02-03 11:54:52 -0500

    Implement easy way check

--- ruby-building-blocks/stock_picker.rb
+++ ruby-building-blocks/stock_picker.rb
@@ -1,10 +1,25 @@
 def stock_picker(prices)
+  # First, check for the easy answer
+  best_buy_price = prices.min
+  best_sell_price = prices.max
+  best_buy_day = prices.index(best_buy_price)
+  best_sell_day = prices.index(best_sell_price)
+  if best_buy_day < best_sell_day
+    return [best_buy_day, best_sell_day]
+  end
+
+  # prices.each_with_index do |price, index|
+  # end
+
+  return 'Easy way failed'
 end
 
-def randomize_stock_prices 
-  stock_prices = Array.new(20) { |price| rand(20) }
-  puts stock_prices.uniq.inspect
-  stock_prices
+def randomize_prices 
+  prices = Array.new(20) { |price| rand(19) + 1 }
+  puts prices.inspect
+  prices
 end
 
-stock_picker(randomize_stock_prices())
+prices = randomize_prices()
+best_days = stock_picker(prices)
+puts best_days.inspect

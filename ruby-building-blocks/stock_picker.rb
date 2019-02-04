3e8c26fe26acdd1b431edc75858a71f618268502
Ricky McCallum <contact@rickymccallum.com>
2019-02-04 14:31:50 -0500

    Begin thoroughly searching for best profit

--- ruby-building-blocks/stock_picker.rb
+++ ruby-building-blocks/stock_picker.rb
@@ -1,26 +1,37 @@
 def stock_picker(prices)
-  # First, check for the easy answer
-  best_buy_price = prices.min
-  best_sell_price = prices.max
-  best_buy_day = prices.index(best_buy_price)
-  best_sell_day = prices.index(best_sell_price)
-  if best_buy_day < best_sell_day
-    return [best_buy_day, best_sell_day]
-  end
+  # # First, check for the easy answer
+  # best_buy_price = prices.min
+  # best_sell_price = prices.max
+  # best_buy_day = prices.index(best_buy_price)
+  # best_sell_day = prices.index(best_sell_price)
+  # if best_buy_day < best_sell_day
+  #   return [best_buy_day, best_sell_day]
+  # end
 
   # Then, go the thorough route
-  prices.each_with_index do |price, index|
+  running_best_profit = 0
+  prices.each_with_index do |buy_price, index|
     # find best profit for this day
       # take slice of days in future
+      future_days = prices.slice(index..-1)
+      puts future_days
       # find max of that
+      best_sell_price = future_days.max
+      # hold onto day
+      best_sell_day = prices.index(best_sell_price)
       # calc best profit
+      todays_best_profit = best_sell_price - buy_price
     # compare to running best profit
       # if greater, replace running best
+      if todays_best_profit > running_best_profit do
+        running_best_profit = todays_best_profit
+        best_buy_day = index
+      end
       # otherwise, discard
   end
 
   # report pair of days that provided best profit
-  return 'Easy way failed'
+  return [best_buy_day, best_sell_day]
 end
 
 def randomize_prices 

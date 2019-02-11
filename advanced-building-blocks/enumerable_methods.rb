7892344744579fcec4ad420eaad1b5bcbbabb779
Ricky McCallum <contact@rickymccallum.com>
2019-02-11 10:06:32 -0500

    my_select

--- advanced-building-blocks/enumerable_methods.rb
+++ advanced-building-blocks/enumerable_methods.rb
@@ -16,6 +16,17 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    i = 0
+    selected_elements = []
+    while i < self.length
+      result = yield(self[i])
+      selected_elements << self[i] if result
+      i += 1
+    end
+    selected_elements
+  end
 end
 
 nums = Array.new(10) { |n| rand(10) }

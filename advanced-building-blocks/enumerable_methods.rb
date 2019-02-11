696e61358d75c85680650c556cbd18c16d613dda
Ricky McCallum <contact@rickymccallum.com>
2019-02-11 12:06:04 -0500

    Remove for from my_each_with_index

--- advanced-building-blocks/enumerable_methods.rb
+++ advanced-building-blocks/enumerable_methods.rb
@@ -2,15 +2,17 @@ module Enumerable
   def my_each
     i = 0
     while i < self.length
-      yield(self[i])
+      yield self[i]
       i += 1
     end
     self
   end
 
   def my_each_with_index
-    for i in 0..self.length - 1
-      yield(self[i], i)
+    i = 0
+    while i < self.length
+      yield self[i], i
+      i += 1
     end
     self
   end

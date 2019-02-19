188aa9eaf070e64b6bc40452af72a9d11a7506e5
Ricky McCallum <contact@rickymccallum.com>
2019-02-19 13:45:53 -0500

    Practice instance method

--- code-quizzes/modules_classes_inheritance.rb
+++ code-quizzes/modules_classes_inheritance.rb
@@ -9,3 +9,14 @@ class Dog
 end
 p fido = Dog.new('Fido')
 p fido.bark
+
+class Insect
+  def initialize(age_in_days)
+    @age_in_days = age_in_days
+  end
+
+  def age_in_years
+    @age_in_days / 365
+  end
+end
+p Insect.new(500).age_in_years

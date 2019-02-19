b2656973df12cb73a18b6cb473cacb02a19fd3a2
Ricky McCallum <contact@rickymccallum.com>
2019-02-19 15:50:21 -0500

    #full_name

--- code-quizzes/modules_classes_inheritance.rb
+++ code-quizzes/modules_classes_inheritance.rb
@@ -39,23 +39,23 @@ class WaterBottle
 end
 p WaterBottle.new(16).size
 
-class Person
-  def initialize(age)
-    @age = age
-  end
+# class Person
+#   def initialize(age)
+#     @age = age
+#   end
 
-  def age=(new_age)
-    @age = new_age
-  end
+#   def age=(new_age)
+#     @age = new_age
+#   end
 
-  def age
-    @age
-  end
-end
-guy = Person.new(20)
-p guy.age
-guy.age = 21
-p guy.age
+#   def age
+#     @age
+#   end
+# end
+# guy = Person.new(20)
+# p guy.age
+# guy.age = 21
+# p guy.age
 
 module MathHelper
   def multiply_by_two(x)
@@ -88,3 +88,18 @@ end
 babe_ruth = BaseballPlayer.new(500, 100, 2_000)
 p babe_ruth.batting_average
 p babe_ruth.on_base_percentage
+
+class Person
+  attr_reader :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
+anon = Person.new('John', 'Smith')
+p anon.full_name

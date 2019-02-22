17a8c828eebc9d2b47df664a961f98c447017c52
Ricky McCallum <contact@rickymccallum.com>
2019-02-22 11:33:59 -0500

    Class planning

---
+++ ruby-oop/tic-tac-toe/tic_tac_toe.rb
@@ -0,0 +1,21 @@
+class TicTacToe
+  def initialize
+    @rounds_played = 0
+    @winner = nil
+    @board = Array.new(9)
+  end
+
+  def play_round
+  end
+
+  def declare_winner
+  end
+
+  def reset_board
+  end
+end
+
+class Player
+  def make_move
+  end
+end
